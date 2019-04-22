23bbde4a3f6ddd41e485d5dc6e5f3221ffbaa355
MattPhillips1 <matt.phillips121@gmail.com>
2019-04-22 11:07:33 +0900

    Add vectors (#5)
    * Add the basic operations for a physics vector
    
    * Add noexcept and finish ops
    
    * Add tests to vector and improve CMake
    
    * Implement div in terms of prod
    
    * Uncapsulate a bit so we can cache if we want
    
    * Cache magnitude so we don't have to compute it all the time
    
    * Better structure on functions

---
+++ mathengine/Vector.cpp
@@ -0,0 +1,97 @@
+#include "Vector.h"
+namespace mathengine {
+	bool PhysVector::operator==(const PhysVector& o) const noexcept {
+		return i() == o.i() && j() == o.j() && k() == o.k();
+	}
+	bool PhysVector::operator!=(const PhysVector& o) const noexcept {
+		return !(*this == o);
+	}
+
+	PhysVector& PhysVector::operator=(const PhysVector& o) noexcept {
+		if (this == &o) return *this;
+		i(o.i());
+		j(o.j());
+		k(o.k());
+		mag_valid = o.mag_valid;
+		mag = o.mag;
+		return *this;
+	}
+
+	// Vector addition
+	PhysVector& PhysVector::operator+=(const PhysVector& o) noexcept {
+		i(i() + o.i());
+		j(j() + o.j());
+		k(k() + o.k());
+		mag_valid = false;
+		return *this;
+	}
+	const PhysVector PhysVector::operator+(const PhysVector& o) const noexcept {
+		return PhysVector(*this) += o;
+	}
+	// Vector subtraction
+	PhysVector& PhysVector::operator-=(const PhysVector& o) noexcept {
+		i(i() - o.i());
+		j(j() - o.j());
+		k(k() - o.k());
+		mag_valid = false;
+		return *this;
+	}
+	const PhysVector PhysVector::operator-(const PhysVector& o) const noexcept {
+		return PhysVector(*this) -= o;
+	}
+
+	// Scalar Multiplication
+	PhysVector& PhysVector::operator*=(const double s) noexcept {
+		i(i() * s);
+		j(j() * s);
+		k(k() * s);
+		if(mag_valid) mag *= s;
+		return *this;
+	}
+	const PhysVector PhysVector::operator*(const double s) const noexcept {
+		return PhysVector(*this) *= s;
+	}
+	const PhysVector operator*(const double k, const PhysVector& v) noexcept {
+		return v*k;
+	}
+
+	// Scalar division
+	PhysVector& PhysVector::operator/=(const double s) {
+		if (s == 0) throw std::runtime_error("Math error: Division by 0");
+		auto div = 1.0/s;
+		return (*this) *= div;
+	}
+	const PhysVector PhysVector::operator/(const double s) const {
+		return PhysVector(*this) /= s;
+	}
+
+	double PhysVector::dot(const PhysVector& o) const noexcept {
+		return i()*o.i() + j()*o.j() + k()*o.k();
+	}
+
+	PhysVector PhysVector::cross(const PhysVector& o) const noexcept {
+		return PhysVector(
+				j()*o.k() - k()*o.j(),
+				i()*o.k() - k()*o.i(),
+				i()*o.j() - j()*o.i());
+	}
+
+	double PhysVector::magnitude() const noexcept {
+		if (!mag_valid){
+			mag = std::sqrt(i()*i() + j()*j() + k()*k());
+			mag_valid = true;
+		}
+		return mag;
+	}
+
+	void PhysVector::normalize() {
+		(*this) /= this->magnitude();
+	}
+
+	PhysVector PhysVector::norm() const {
+		auto temp = PhysVector(*this);
+		temp.normalize();
+		return temp;
+	}
+
+};
---
+++ mathengine/Vector.h
@@ -0,0 +1,62 @@
+#ifndef P_VECTOR
+#define P_VECTOR
+
+#include <cmath>
+#include <stdexcept>
+
+namespace mathengine {
+	class PhysVector {
+		public:
+			PhysVector() noexcept : _i{0}, _j{0}, _k{0}, mag{0}, mag_valid{true}{}
+			PhysVector(double i, double j, double k) noexcept : _i{i}, _j{j}, _k{k}, mag{0}, mag_valid{false}{}
+			PhysVector(const PhysVector& o) noexcept : _i{o.i()}, _j{o.j()}, _k{o.k()}, mag{o.mag}, mag_valid{o.mag_valid}{}
+			PhysVector(PhysVector&& o) noexcept : _i{o.i()}, _j{o.j()}, _k{o.k()}, mag{o.mag}, mag_valid{o.mag_valid} {}
+			PhysVector& operator=(const PhysVector& o) noexcept;
+			bool operator==(const PhysVector& o) const noexcept;
+			bool operator!=(const PhysVector& o) const noexcept;
+
+			// Vector addition
+			PhysVector& operator+=(const PhysVector& o) noexcept;
+			const PhysVector operator+(const PhysVector& o) const noexcept;
+
+			// Vector subtraction
+			PhysVector& operator-=(const PhysVector& o) noexcept;
+			const PhysVector operator-(const PhysVector& o) const noexcept;
+
+			// Scalar Multiplication
+			PhysVector& operator*=(const double s) noexcept;
+			const PhysVector operator*(const double s) const noexcept;
+			friend const PhysVector operator*(const double k, const PhysVector& v) noexcept;
+
+			// Scalar division
+			PhysVector& operator/=(const double s);
+			const PhysVector operator/(const double s) const;
+
+			double dot(const PhysVector& o) const noexcept;
+
+			PhysVector cross(const PhysVector& o) const noexcept;
+
+			double magnitude() const noexcept;
+
+			void normalize();
+
+			PhysVector norm() const;
+
+			inline double i() const noexcept { return _i; }
+			inline double j() const noexcept { return _j; }
+			inline double k() const noexcept { return _k; }
+
+			inline void i(const double n) noexcept { mag_valid = false; _i = n; }
+			inline void j(const double n) noexcept { mag_valid = false; _j = n; }
+			inline void k(const double n) noexcept { mag_valid = false; _k = n; }
+
+
+		private:
+			double _i;
+			double _j;
+			double _k;
+			mutable double mag;
+			mutable bool mag_valid;
+	};
+};
+#endif
