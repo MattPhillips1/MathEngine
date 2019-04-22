0540c05899e121af1cb84aca6afe37600d9639a6
Matthew <matt.phillips121@gmail.com>
2019-04-23 08:43:51 +0900

    Add matrix and some tests

--- mathengine/Matrix.cpp
+++ mathengine/Matrix.cpp
@@ -2,4 +2,123 @@
 
 namespace mathengine {
 
+	bool PhysMatrix::operator==(const PhysMatrix& rhs) const noexcept {
+		return rows == rhs.rows;
+	}
+
+	bool PhysMatrix::operator!=(const PhysMatrix& rhs) const noexcept {
+		return !(*this == rhs);
+	}
+	// Matrix Addition
+	PhysMatrix& PhysMatrix::operator+=(const PhysMatrix& rhs) noexcept {
+		for (unsigned int i = 0; i < 3; ++i){
+			rows[i] += rhs[i];
+		}
+		return (*this);
+	}
+	const PhysMatrix PhysMatrix::operator+(const PhysMatrix& rhs) noexcept {
+		return PhysMatrix(*this) += rhs;
+	}
+
+	// Matrix Subtraction
+	PhysMatrix& PhysMatrix::operator -= (const PhysMatrix& rhs) noexcept {
+		for (unsigned int i = 0; i < 3; ++i){
+			rows[i] -= rhs[i];
+		}
+		return (*this);
+	}
+	const PhysMatrix PhysMatrix::operator-(const PhysMatrix& rhs) noexcept {
+		return PhysMatrix(*this) -= rhs;
+	}
+
+	// Scalar Multiplication
+	PhysMatrix& PhysMatrix::operator*=(const double s) noexcept {
+		for (auto& r : rows){
+			r *= s;
+		}
+		return (*this);
+	}
+	const PhysMatrix PhysMatrix::operator*(const double s) const noexcept {
+		return PhysMatrix(*this) *= s;
+	}
+	const PhysMatrix operator*(const double s, const PhysMatrix& v) noexcept {
+		return v*s;
+	}
+
+	// Scalar Division
+	PhysMatrix& PhysMatrix::operator/=(const double s) {
+		for (auto& row : rows){
+			row /= s;
+		}
+		return (*this);
+	}
+	const PhysMatrix PhysMatrix::operator/(const double s) const {
+		return PhysMatrix(*this) /= s;
+	}
+
+	// Vector & Matrix Multiplication
+	const PhysVector PhysMatrix::operator*(const PhysVector& rhs) const noexcept {
+		auto i = (*this)[0].dot(rhs);
+		auto j = (*this)[1].dot(rhs);
+		auto k = (*this)[2].dot(rhs);
+		return PhysVector(i,j,k);
+	}
+	PhysMatrix& PhysMatrix::operator*=(const PhysMatrix& rhs) noexcept {
+		auto col1 = rhs.i();
+		auto col2 = rhs.j();
+		auto col3 = rhs.k();
+
+		auto make_row = [&](const auto& row){
+			return PhysVector(row.dot(col1), row.dot(col2), row.dot(col3));
+		};
+		rows[0] = make_row(rows[0]);
+		rows[1] = make_row(rows[1]);
+		rows[2] = make_row(rows[2]);
+		return (*this);
+	}
+	const PhysMatrix PhysMatrix::operator*(const PhysMatrix& rhs) const noexcept {
+		return PhysMatrix(*this) *= rhs;
+	}
+
+	// Matrix Operations
+	void PhysMatrix::transpose() noexcept {
+		double tmp = rows[0].j();
+		rows[0].j(rows[1].i());
+		rows[1].i(tmp);
+
+		tmp = rows[0].k();
+		rows[0].k(rows[2].i());
+		rows[2].i(tmp);
+
+		tmp = rows[1].k();
+		rows[1].k(rows[2].j());
+		rows[2].j(tmp);
+	}
+	PhysMatrix PhysMatrix::transpose_of() const noexcept {
+		return PhysMatrix(i(), j(), k());
+	}
+	inline double cross_part(const double& k, const double& c1, const double& c2, const double& c3, const double& c4) noexcept {
+		return k*(c1*c4 - c2*c3);
+	}
+	PhysMatrix PhysMatrix::inverse_of() const {
+		auto make_vec = [&](const int start, const unsigned int r1, const unsigned int r2){
+			double i = cross_part(start, rows[r1].j(), rows[r1].k(), rows[r2].j(), rows[r2].k());
+			double j = cross_part(start*-1, rows[r1].i(), rows[r1].k(), rows[r2].i(), rows[r2].k());
+			double k = cross_part(start, rows[r1].i(), rows[r1].j(), rows[r2].i(), rows[r2].j());
+			return PhysVector(i,j,k);
+		};
+		auto r1 = make_vec(1,1,2);
+		auto r2 = make_vec(-1,0,2);
+		auto r3 = make_vec(1,0,1);
+
+		double det = rows[0].i() * r1.i() + rows[0].j() * r1.j() + rows[0].k() * r1.k();
+
+		return PhysMatrix(r1,r2,r3)/det;
+	}
+	double PhysMatrix::det() const noexcept {
+		double d = cross_part(rows[0].i(), rows[1].j(), rows[1].k(), rows[2].j(), rows[2].k());
+		d -= cross_part(rows[0].j(), rows[1].i(), rows[1].k(), rows[2].i(), rows[2].k());
+		d += cross_part(rows[0].k(), rows[1].i(), rows[1].j(), rows[2].i(), rows[2].j());
+		return d;
+	}
 }
--- mathengine/Matrix.h
+++ mathengine/Matrix.h
@@ -16,6 +16,53 @@ namespace mathengine {
 		*/
 		public:
 			// Default, Copy, Move constructors generated
+			PhysMatrix() = default;
+			PhysMatrix(PhysVector&& a, PhysVector&& b, PhysVector&& c) : rows{a,b,c} {}
+			PhysMatrix(
+					const double i1, const double j1, const double k1,
+					const double i2, const double j2, const double k2,
+					const double i3, const double j3, const double k3
+					) : PhysMatrix(PhysVector{i1,j1,k1}, PhysVector{i2,j2,k2}, PhysVector{i3,j3,k3}) {}
+			PhysMatrix(const PhysVector& a, const PhysVector& b, const PhysVector& c) : rows{a,b,c} {}
+
+			bool operator==(const PhysMatrix& rhs) const noexcept;
+			bool operator!=(const PhysMatrix& rhs) const noexcept;
+
+			// Matrix Addition
+			PhysMatrix& operator += (const PhysMatrix& rhs) noexcept;
+			const PhysMatrix operator+(const PhysMatrix& rhs) noexcept;
+
+			// Matrix Subtraction
+			PhysMatrix& operator -= (const PhysMatrix& rhs) noexcept;
+			const PhysMatrix operator-(const PhysMatrix& rhs) noexcept;
+
+			// Scalar Multiplication
+			PhysMatrix& operator*=(const double s) noexcept;
+			const PhysMatrix operator*(const double s) const noexcept;
+			friend const PhysMatrix operator*(const double s, const PhysMatrix& v) noexcept;
+
+			// Scalar Division
+			PhysMatrix& operator/=(const double s);
+			const PhysMatrix operator/(const double s) const;
+
+			// Vector & Matrix Multiplication
+			const PhysVector operator*(const PhysVector& rhs) const noexcept;
+			PhysMatrix& operator*=(const PhysMatrix& rhs) noexcept;
+			const PhysMatrix operator*(const PhysMatrix& rhs) const noexcept;
+
+			PhysVector& operator[](const unsigned int i) { return rows[i]; }
+			PhysVector operator[](const unsigned int i) const { return rows[i]; }
+			PhysVector col(const unsigned int i) const;
+			PhysVector i() const { return PhysVector(rows[0].i(), rows[1].i(), rows[2].i()); }
+			PhysVector j() const { return PhysVector(rows[0].j(), rows[1].j(), rows[2].j()); }
+			PhysVector k() const { return PhysVector(rows[0].k(), rows[1].k(), rows[2].k()); }
+
+			// Matrix Operations
+			void transpose() noexcept;
+			PhysMatrix transpose_of() const noexcept;
+			PhysMatrix inverse_of() const;
+			double det() const noexcept;
+
 
 		private:
 			std::array<PhysVector, 3> rows;
